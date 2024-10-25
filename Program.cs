f622679fa19412f6b76c5085cf08851817d9557b
GLi <guangyanli5@gmail.com>
2024-10-25 13:02:17 -0700

    final udpate 10/25

--- Program.cs
+++ Program.cs
@@ -14,6 +14,18 @@
             Time.WriteTime2(t2);
             t1.WriteTime3();
             t2.WriteTime3();
+            Console.WriteLine("let's invoke WriteTime4:");
+            
+            t1.WriteTime4(t1);
+            t1.WriteTime4(t2);
+            t2.WriteTime4(t1);
+            t2.WriteTime4(t2);
+            /*
+        public void WriteTime4(Time t)
+        {   // a version takes an argument
+            Console.WriteLine(t.hour + ":" + t.minute + ":" + t.second);
+        }
+            */
         }
     }
 }
