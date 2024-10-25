427713003976afdedb5890dbd246d68df76fd4f1
GLi <guangyanli5@gmail.com>
2024-10-25 12:58:18 -0700

    update

--- Program.cs
+++ Program.cs
@@ -5,19 +5,15 @@
         static void Main(string[] args)
         {
             Time t1 = new Time();
-            Console.WriteLine(t1);
-            Console.WriteLine(t1.hour);
-            Console.WriteLine(t1.minute);
-            Console.WriteLine(t1.second);
-
+            
             Time t2 = new Time(11, 23, 55);
-            Console.WriteLine(t2.hour);
-            Console.WriteLine(t2.minute);
-            Console.WriteLine(t2.second);
+            
 
             Console.WriteLine($"{t2.hour}:{t2.minute}:{t2.second}");
             Time.WriteTime(t2);
-            t1.WriteTime2(t1);
+            Time.WriteTime2(t2);
+            t1.WriteTime3();
+            t2.WriteTime3();
         }
     }
 }
--- Time.cs
+++ Time.cs
@@ -26,10 +26,21 @@ namespace TimeProj
             Console.WriteLine(t.hour + ":" + t.minute + ":" + t.second);
         }
 
-        public void WriteTime2(Time t)
-        {
+        public static void WriteTime2(Time t)
+        {   // a version that uses String.Format
+            Console.WriteLine(String.Format("{0:D2}:{1:D2}:{2:D2}",
+            t.hour, t.minute, (int)t.second));
+        }
+
+        public void WriteTime3()
+        {  // a version uses this
+            Console.WriteLine(this.hour + ":" + this.minute + ":" + this.second);
+        }
+        public void WriteTime4(Time t)
+        {   // a version takes an argument
             Console.WriteLine(t.hour + ":" + t.minute + ":" + t.second);
         }
+
 
     }
 }
